6319f478f6e3ee99bef1137f12654f37e878a5f4
Roman Dogoda <piterom911@gmail.com>
2021-07-26 14:58:13 +0300

    with hashRouter

--- src/p1-main/m1-ui/u1-app/App.tsx
+++ src/p1-main/m1-ui/u1-app/App.tsx
@@ -4,18 +4,21 @@ import HW1 from '../../../p2-homeworks/h1/HW1'
 import HW2 from "../../../p2-homeworks/h2/HW2";
 import HW3 from '../../../p2-homeworks/h3/HW3';
 import HW4 from "../../../p2-homeworks/h4/HW4";
+import HW5 from "../../../p2-homeworks/h5/HW5";
+import {HashRouter} from "react-router-dom";
 
 function App() {
     return (
-        <div className={s.App}>
-            <h1 className={s.heading}>react homeworks:</h1>
-            <HW4/>
-            <HW1/>
-            <HW2/>
-            <HW3/>
-            {/*<HW5/>*/}
-
-        </div>
+        <HashRouter>
+            <div className={s.App}>
+                <h1 className={s.heading}>react homeworks:</h1>
+                <HW4/>
+                <HW1/>
+                <HW2/>
+                <HW3/>
+                <HW5/>
+            </div>
+        </HashRouter>
     )
 }
 
--- src/p2-homeworks/h5/HW5.tsx
+++ src/p2-homeworks/h5/HW5.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { HashRouter } from 'react-router-dom'
 import Header from './Header'
 import Routes from './Routes'
 
@@ -10,7 +11,7 @@ function HW5() {
 
             <Header/>
 
-            <Routes/>
+            {/*<Routes/>*/}
 
             {/*</HashRouter>*/}
         </div>
